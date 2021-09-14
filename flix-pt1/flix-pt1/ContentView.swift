3e649e88a283653e4014617c8b37ca7efbf72d5f
laurachangufl <73550815+laurachangufl@users.noreply.github.com>
2021-09-13 21:07:33 -0400

    start assignment 1

---
+++ flix-pt1/flix-pt1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  flix-pt1
+//
+//  Created by Laura Chang on 9/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
