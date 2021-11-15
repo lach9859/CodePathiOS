f5bbdc4aa2252c5d1043200f08b52bd85fffb0f0
laurachangufl <73550815+laurachangufl@users.noreply.github.com>
2021-11-14 20:54:57 -0500

    moved spice to proper repository

--- Spice/Pods/Target Support Files/Pods-Spice-SpiceUITests/Pods-Spice-SpiceUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Spice_SpiceUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Spice_SpiceUITests
-@end
--- Spice/Pods/Target Support Files/Pods-Spice/Pods-Spice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Spice : NSObject
-@end
-@implementation PodsDummy_Pods_Spice
-@end
