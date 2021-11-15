d3e9f86a8e8987390b0cdc74a7a6143128215130
laurachangufl <73550815+laurachangufl@users.noreply.github.com>
2021-11-14 19:29:17 -0500

    parstagram and spice beginning

---
+++ Spice/Spice/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.swift
+//  Spice
+//
+//  Created by Laura Chang on 11/14/21.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { user, error in
+            if (user != nil) {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Spice/Spice/RegisterViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RegisterViewController.swift
+//  Spice
+//
+//  Created by Laura Chang on 11/14/21.
+//
+
+import UIKit
+import Parse
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneNumberField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.email = emailField.text
+        // other fields can be set just like with PFObject
+        user["phone"] = phoneNumberField.text
+
+        user.signUpInBackground { success, error in
+            if success { // can also use if error != nil
+                self.performSegue(withIdentifier: "registerSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
